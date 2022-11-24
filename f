a32de28dd8f1d6df4055be22892e988c29ffddee
Roberto Arce <roberto_arce_@hotmail.com>
2022-11-24 13:11:27 +0100

    adding new config and files

---
+++ f
@@ -0,0 +1,1 @@
+# Created by Octave 6.1.0, Mon Feb 01 18:02:29 2021 GMT <unknown@My-baby-Titorium>
--- test/suma.m
+++
@@ -1,7 +0,0 @@
-function [ b ] = sum( x,y )
-%SUM Summary of this function goes here
-%   Detailed explanation goes here
-
-b=x+y;
-end
-
